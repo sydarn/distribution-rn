1f169e3e61cff3e8bb11b9f562f3e78a968d0e0e
sydarn <sydarn@proton.me>
2024-07-07 14:52:35 +0200

    fake numa
    needs numa=fake=4 on the kernel commands

---
+++ packages/sysutils/numactl/package.mk
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024 ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="numactl"
+PKG_VERSION="8c454ecb0e274d254a9393346297936e67ef9e05"
+PKG_LICENSE="GPL2"
+PKG_URL="https://github.com/${PKG_NAME}/${PKG_NAME}/archive/${PKG_VERSION}.zip"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="NUMA support for Linux"
+PKG_TOOLCHAIN="autotools"
+
+
